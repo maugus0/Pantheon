b0e7b0e2b9721be6aefdfbac531c666e52c0660f
Ahan Jaiswal <63398617+maugus0@users.noreply.github.com>
2022-02-04 09:16:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php include "root/db.php" ?>
+<?php include "root/header.php" ?>
+	<!-- Preloader -->
+	<div class="preeloader">
+		<div class="preloader-spinner"></div>
+	</div>
+	<!-- End Preloader -->
+	
+	<!-- Color Plate -->
+	<div class="color-plate ">
+		<a class="color-plate-icon"><i class="fa fa-cog fa-spin"></i></a>
+		<h4>Pantheon Deepsight</h4>
+		<p>Here are same other awesome theme colors.</p>
+		<span class="color1"></span>
+		<span class="color2"></span>
+		<span class="color3"></span>
+		<span class="color4"></span>
+	</div>
+	<!-- /End Color Plate -->
+    <!--Navigation-->
+	<?php include "root/nav.php" ?>
+	<!--Hero header start-->
+    <section class="hero-header primary-header slider-header" id="home">
+        <div class="container">
+			<div class="row">
+				<div class="col-lg-5 col-12">
+					<div class="hero-header-content">
+					    <p><b>Join Pantheon if you already haven't!</b></p>
+					    <h1>Welcome Back to   <b>Pantheon.</b></h1>
+					    <p style="font-size: large;">
+                            A simple, fun & creative way to capture, share & post photos, information & messages with the community.<br>
+                            We strive to bring technical people from all fields together in a safe and supportive community.
+					    </p>
+					    <div class="button">
+							<a href="login.php" class="btn primary"><i class="lni-play"></i> Login</a>
+							<a href="register.php" class="btn btn-secondary">Register</a>
+					    </div>
+					</div>
+				</div>
+				<div class="col-lg-7 col-12">
+					<div class="hero-header-image">
+					   <img class="img-responsive" src="img/hero-image.jpg" alt="#">
+					</div>
+				</div>
+			</div>
+        </div>
+    </section>
+    <!--Hero header end-->
+    <!--Footer-->
+    <?php include "root/footer.php" ?>
+	
+	
